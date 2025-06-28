6d907bf7404f2b6124950f83b66bd5139b1ed575
Sulabh Katila <sulabhkatila@gmail.com>
2025-06-28 16:41:54 -0400

    client2

--- basic/client/client.go
+++ basic/client/client.go
@@ -3,40 +3,41 @@ package main
 import (
 	"fmt"
 	"net"
-	"strconv"
+	"os"
 )
 
-var PORT = 8000
+const (
+	NETWORKTYPE = "tcp4"
+	HOSTADDRESS = "localhost"
+	PORT        = "8000"
+)
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.0:"+strconv.Itoa(PORT))
-	if err != nil {
-		fmt.Println("failed to connect")
-		return
-	}
+	conn, err := net.Dial(NETWORKTYPE, HOSTADDRESS+":"+PORT)
+	exitIfError(err)
 
 	fmt.Println("Connected")
 	go handleConnection(conn)
 }
 
 func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
 	req := "Hello"
 	_, err := conn.Write([]byte(req))
-	if err != nil {
-		fmt.Println("Failed to write")
-		conn.Close()
-		return
-	}
+	exitIfError(err)
+
 	fmt.Println("Wrote: ", req)
 
 	var res string
-	_, error := conn.Read([]byte(res))
-	if error != nil {
-		fmt.Println("Failed to read")
-		conn.Close()
-		return
-	}
+	_, err = conn.Read([]byte(res))
+	exitIfError(err)
 
 	fmt.Println("read: ", res)
-	conn.Close()
+}
+
+func exitIfError(err error) {
+	if err != nil {
+		os.Exit(1)
+	}
 }
